f1c0c1041a7822be1f1989ee7f7ecbc455107102
LJKR2020 <loredana.rietveld@gmail.com>
2021-01-25 23:19:04 +0100

    eerste test onder begeleiding van lkr gemaakt

--- index.test.js
+++ index.test.js
@@ -1,5 +1,22 @@
 // test uit de les
 
-test("even kijken of dit werkt", () => {
+function add(number1, number2) {
+    console.log("INPUT", number1, number2);
+    const sum = number1 + number2;
+    console.log("SUM", sum);
+    return sum
+}
+
+test("add() function should return the sum of 2 numbers", () => {
     console.log("Hallo")
+    // ARRANGE
+    const number1 = 4;
+    const number2 = 5;
+
+    // ACT
+    const sum = add(number1, number2);
+
+    // ASSERT
+    expect(sum).toBe(9);
 })
+
